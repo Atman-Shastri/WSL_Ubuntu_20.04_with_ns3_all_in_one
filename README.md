a5cfc25aff2759099af91ffde4998c80a13ce7dc
Atman-Shastri <atmanshastri@gmail.com>
2023-06-01 17:41:06 +0530

    Commit Before testing on fresh Windows 10 & 11

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-"# WSL_Ubuntu_20.04_with_ns3_all_in_one" 
-Get-Content .\download.ps1 | powershell -noprofile -
+1. Run Powershell as administrator and cd to the directory where the repo folder is downloaded/cloned
+2. Run the command given below:
+powershell.exe -ExecutionPolicy Bypass -File .\ns-3.32_Ubuntu-20.04_Download_and_Install.ps1
+3. The password for the OS is the same as you use in lab: "mca@123"
 
 If the script fails to download, alternatively you can download the OS from here:
 https://drive.google.com/file/d/1by4Ou977Wtm0I5OFzQRX1vBx1Fs283re/view?usp=drive_link
 
+Download size is 1.82 gb, and the OS will reside in C:\TIMSCDR-Ubuntu-20.04, consuming 7 gb storage space
--- ns-3.32_Ubuntu-20.04_Download_and_Install.ps1
+++ ns-3.32_Ubuntu-20.04_Download_and_Install.ps1
@@ -1,35 +1,36 @@
     # This script downloads Ubuntu with pre-installed programs related to MCA Program course 22-24 #
                        # (includes ns3-allinone and tracemetrics) #
 
-                        <# Downloading TIMSCDR Ubuntu WSL machine #>
-      
-# Creating script's working directory#        
+
+<# Downloading TIMSCDR Ubuntu WSL machine #>
+
+# Creating script's working directory #        
 $trashVar = mkdir -OutVariable output C:\temporary\
 $trashVar
+
 # Downloading wget & 7-zip binaries to C:\temporary #
 Write-Progress -Activity 'Downloading wget & 7-zip binaries to C:\temporary' -Status "Saving to folder" -PercentComplete 75
 $ProgressPreference = "SilentlyContinue"
 Invoke-WebRequest https://eternallybored.org/misc/wget/1.21.4/64/wget.exe -O "C:\temporary\wget.exe"
 Invoke-WebRequest https://www.7-zip.org/a/7zr.exe -O "C:\temporary\7zr.exe"
-
 Write-Progress -Activity 'Downloading ns3.32-Ubuntu-20.04' -Status 'Downloading'
+
 # Downloading TIMSCDR-Ubuntu-20-04.7z to C:\temporary folder" #
-
 Write-Host "Download size is 1.82 gb, and the OS will reside in C:\TIMSCDR-Ubuntu-20.04, consuming 7 gb storage space"
-
 C:\temporary\wget.exe -t 0 -O "C:\temporary\TIMSCDR-Ubuntu-20.04.7z" "https://media.githubusercontent.com/media/Atman-Shastri/WSL_Ubuntu_20.04_with_ns3_all_in_one/main/TIMSCDR-Ubuntu-20.04.7z"
-
 Write-Host "If Script fails to download the OS, download it from here: https://drive.google.com/file/d/1by4Ou977Wtm0I5OFzQRX1vBx1Fs283re/view?usp=drive_link"
 
-### Extracting TIMSCDR-Ubuntu-20.04.7z to the C:\temporary ###
+# Extracting TIMSCDR-Ubuntu-20.04.7z to the C:\temporary #
 C:\temporary\7zr.exe x "C:\temporary\TIMSCDR-Ubuntu-20.04.7z" -o"C:\temporary" -y
 
-                                    <# Checking and enabling pre-requisites #>
+
+<# Checking and enabling pre-requisites (WSL and Virtual Machine Platform) #>
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 
 
-
-                                  <# Setting up profile in windows Terminal #>
-<# Path to the JSON file
+<# Setting up profile in windows Terminal #>
+# Path to the JSON file #
 $jsonFilePath = "C:\Users\$env:USERNAME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
 
@@ -69,10 +70,10 @@ $colorScheme = @{
 # Read the JSON file contents
 $jsonContent = Get-Content -Path $jsonFilePath | ConvertFrom-Json
 
-#appending color scheme
+#appending color ubuntu scheme
 $jsonContent.schemes += $colorScheme
 
-#appending new profile
+#appending new ubuntu profile
 $jsonContent.profiles.list += $newProfile
 
 # Converting the updated JSON content back to a string
@@ -84,3 +85,9 @@ $jsonString | Out-File -FilePath $jsonFilePath -Encoding utf8 -Force
 <# ignore this line, its for testing purposes
 cp C:\Users\$env:USERNAME\Desktop\DELL-G3-wt-og.json C:\Users\$env:USERNAME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 #>
+
+<# Importing the OS #>
+wsl --import TIMSCDR-Ubuntu-20.04 C:\TIMSCDR-Ubuntu-20.04 C:\temporary\TIMSCDR-Ubuntu-20.04.tar
+
+<# Cleaning up, removing C:\temporary #>
+Remove-Item -r C:\temporary
