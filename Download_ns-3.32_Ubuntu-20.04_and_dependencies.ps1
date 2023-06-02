b130577426535ea7cb86c4286709d263b8beee93
Atman Shastri <126988436+Atman-Shastri@users.noreply.github.com>
2023-06-02 14:29:03 +0530

    Prompt changes

--- Download_ns-3.32_Ubuntu-20.04_and_dependencies.ps1
+++ Download_ns-3.32_Ubuntu-20.04_and_dependencies.ps1
@@ -6,10 +6,9 @@
 
 # Creating script's working directory #        
 $trashVar = mkdir -OutVariable output C:\temporary\
-$trashVar
 
 # Downloading wget & 7-zip binaries to C:\temporary #
-Write-Host "`tDownloading wget & 7-zip binaries to C:\temporary`n" -ForegroundColor Yellow
+Write-Host "`tDownloading wget & 7-zip binaries to C:\temporary" -ForegroundColor Yellow
 $ProgressPreference = "SilentlyContinue"
 Invoke-WebRequest https://eternallybored.org/misc/wget/1.21.4/64/wget.exe -O "C:\temporary\wget.exe"
 Invoke-WebRequest https://www.7-zip.org/a/7zr.exe -O "C:\temporary\7zr.exe"
@@ -42,13 +41,13 @@ $wtProcessId = $wtProcess.Id
 # Stopping Windows Terminal using the process ID
 Stop-Process -Id $wtProcessId
 
-Write-Host "`n`tDownload size is 1.82 gb, and the OS will reside in C:\TIMSCDR-Ubuntu-20.04, consuming 7 gb storage space`n" -ForegroundColor Yellow
-Write-Host "`n`tYou can download it manually from here in case the script fails to download the OS`n" -ForegroundColor Yellow
+Write-Host "`n`tDownload size is 1.82 gb, and the OS will reside in C:\TIMSCDR-Ubuntu-20.04, consuming 7 gb storage space" -ForegroundColor Yellow
+Write-Host "`n`tYou can download it manually from here in case the script fails to download the OS" -ForegroundColor Yellow
 Write-Host "`n`thttps://drive.google.com/file/d/1I1rJfOcfIffNtPC5M3Qx-IPVIw5FgYQo/view?usp=drive_link" -ForegroundColor Yellow
 
 
 # Downloading TIMSCDR-Ubuntu-20-04.7z to C:\temporary folder" #
-Write-Host "`tDownloading the ns3.32-Ubuntu-20.04 from OneDrive`n" -ForegroundColor Yellow
+Write-Host "`t`nDownloading the ns3.32-Ubuntu-20.04 from OneDrive`n" -ForegroundColor Yellow
 C:\temporary\wget.exe  -t 0 -O "C:\temporary\TIMSCDR-Ubuntu-20.04.7z" "https://ktokpa.sn.files.1drv.com/y4meXurSB3sCLcGjmCoWxCa2sQkgUtmQkuJ5iAbYwilWi_JCnSDMl3-0N73vDm9PoOgWu5JPzm7vAgBiUKobniW4eDKciHO9c7YMJb2dhPaVUS-h00k4xc5Cfe_8gYK20WvSwlIsofXUtbNdGBITTeNjD7H-8e47JkGlPg7mLIgxwcbw48z7yWZkIi2_KWmYhZ_sjHMAY9pb27nIZZ1deZHZA"
 
 # Extracting TIMSCDR-Ubuntu-20.04.7z to the C:\temporary #
