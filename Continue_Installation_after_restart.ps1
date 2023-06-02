ee69274b1d742e890a0f11735dbf170377bf535c
Atman-Shastri <atmanshastri@gmail.com>
2023-06-03 02:04:44 +0530

    03-06-2023 02:04

--- Continue_Installation_after_restart.ps1
+++ Continue_Installation_after_restart.ps1
@@ -4,9 +4,10 @@ Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
 
 <# Importing the OS #>
 Write-Host "`tImporting the OS from C:\temporary to C:\TIMSCDR-Ubuntu-20.04`n" -ForegroundColor Yellow
+Write-Host "`tThis can take about 10-15 minutes, please be patient`n" -ForegroundColor Yellow
 wsl --import TIMSCDR-Ubuntu-20.04 C:\TIMSCDR-Ubuntu-20.04 C:\temporary\TIMSCDR-Ubuntu-20.04.tar
 
-<# Making sure windows terminal profile creation is accurate#
+<# Making sure windows terminal profile creation is accurate#>
 Copy-Item "C:\Users\$env:USERNAME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" "C:\Users\$env:USERNAME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json.bak"
 Copy-Item "C:\temporary\settings.json" "C:\Users\$env:USERNAME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
 
