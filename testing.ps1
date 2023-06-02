a3c3b9f3ead0b3e6c5b4339d2a2985bf17549a3d
Atman-Shastri <atmanshastri@gmail.com>
2023-06-02 21:51:45 +0530

    Rethinking life choices

--- testing.ps1
+++ testing.ps1
@@ -1,4 +1,4 @@
-# This script downloads Ubuntu 20.04 with pre-installed programs related to networking with linux (MCA Program course 22-24) #
+﻿# This script downloads Ubuntu 20.04 with pre-installed programs related to networking with linux (MCA Program course 22-24) #
 # (includes ns3-allinone and tracemetrics) #
 
 
