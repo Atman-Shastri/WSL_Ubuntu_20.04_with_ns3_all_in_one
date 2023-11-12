5f95775244235fbc24fbec631d1090b58d773828
Atman Shastri <126988436+Atman-Shastri@users.noreply.github.com>
2023-11-12 22:20:35 +0530

    Update Download_ns-3.32_Ubuntu-20.04_and_dependencies.ps1

--- Download_ns-3.32_Ubuntu-20.04_and_dependencies.ps1
+++ Download_ns-3.32_Ubuntu-20.04_and_dependencies.ps1
@@ -7,17 +7,47 @@ $choice1 = Read-Host -Prompt "Press y then enter after you read the above displa
 
 if($choice1 -eq 'y'){
 
-<# Downloading ns3 Ubuntu WSL machine #>
+function DownloadFileAndSkipIfExists {
+    param (
+        [string]$url,
+        [string]$destinationPath
+    )
 
-# Creating script's working directory #        
-$trashVar = mkdir -OutVariable output C:\temporary\
+    if (Test-Path $destinationPath) {
+        Write-Host "File already exists at $destinationPath. Skipping the download."
+        return
+    }
+
+    try {
+        Invoke-WebRequest -Uri $url -OutFile $destinationPath
+        Write-Host "File downloaded successfully to $destinationPath."
+    }
+    catch {
+        Write-Host "Error downloading the file: $($_.Exception.Message)"
+        
+    }
+}
+
+<# Downloading ns3 Ubuntu WSL machine #>
+$directoryPath = "C:\temporary"
+
+# Create the directory
+try {
+    $output = New-Item -ItemType Directory -Path $directoryPath -ErrorAction Stop
+    Write-Host "Directory created successfully: $($output.FullName)"
+}
+catch {
+    Write-Host "Error creating directory: $($_.Exception.Message)"
+    # You can add more specific error handling if needed
+}
 
 # Downloading wget & 7-zip binaries to C:\temporary #
 Write-Host "`tDownloading wget & 7-zip binaries to C:\temporary" -ForegroundColor Yellow
 $ProgressPreference = "SilentlyContinue"
-Invoke-WebRequest https://eternallybored.org/misc/wget/1.21.4/64/wget.exe -O "C:\temporary\wget.exe"
-Invoke-WebRequest https://www.7-zip.org/a/7zr.exe -O "C:\temporary\7zr.exe"
+DownloadFileAndSkipIfExists -url https://eternallybored.org/misc/wget/1.21.4/64/wget.exe -destinationPath "C:\temporary\wget.exe"
+DownloadFileAndSkipIfExists -url https://www.7-zip.org/a/7zr.exe -destinationPath "C:\temporary\7zr.exe"
 
+try{
 # Downloading and installing Windows Terminal if it does not exist #
 Write-Host "`tDownloading and installing Windows Terminal if it does not exist`n" -ForegroundColor Yellow
 C:\temporary\wget.exe -t 0 -O "C:\temporary\winget.msixbundle" "https://github.com/microsoft/winget-cli/releases/download/v1.4.11071/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
@@ -26,110 +56,93 @@ C:\temporary\wget.exe -t 0 -O "C:\temporary\VCLibs.appx" "https://aka.ms/Microso
 C:\temporary\wget.exe -t 0 -O "C:\temporary\UIXAML.zip" "https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.3"
 # Downloading settings.json for Windows terminal #
 C:\temporary\wget.exe -t 0 -O "C:\temporary\settings.json" "https://raw.githubusercontent.com/Atman-Shastri/WSL_Ubuntu_20.04_with_ns3_all_in_one/main/settings.json"
+
 Expand-Archive "C:\temporary\UIXAML.zip" -DestinationPath "C:\temporary\UIXAML\"
 Copy-Item "C:\temporary\UIXAML\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx" "C:\temporary\UIXAML.appx"
+
 Add-AppPackage "C:\temporary\UIXAML.appx"
 Add-AppPackage "C:\temporary\VCLibs.appx"
 Add-AppxPackage "C:\temporary\terminal.msixbundle"
 Add-AppPackage "C:\temporary\winget.msixbundle"
+} catch {}
 
 # Testing Windows Terminal for Profile Creation #
 Write-Host "`tTesting Windows Terminal for Profile Creation`n" -ForegroundColor Yellow
 # Starting Windows Terminal
-Start-Process wt
-
-# Waiting for 2 seconds
-Start-Sleep -Seconds 2
-
-# Finding the process ID for Windows Terminal
-$wtProcess = Get-Process | Where-Object { $_.Path -like '*WindowsTerminal.exe' }
-$wtProcessId = $wtProcess.Id
-
-# Stopping Windows Terminal using the process ID
-Stop-Process -Id $wtProcessId
+Start-Process wt -WindowStyle Hidden
 
 
 Write-Host "`n`tDownload size is 1.82 gb, and the OS will reside in C:\ns3-Ubuntu-20.04, consuming 7 gb storage space" -ForegroundColor Yellow
 
+$directoryPath = "C:\temporary"
+$fileName = "ns3-Ubuntu-20.04.7z"
+$fileExists = Test-Path -Path (Join-Path $directoryPath $fileName)
+if (-not $fileExists) {
+    # Execute this code if the file doesn't exist
+    Write-Host "File does not exist. Proceeding with the code."
 
-# Downloading ns3-Ubuntu-20-04.7z to C:\temporary folder" #
+    # Downloading ns3-Ubuntu-20-04.7z to C:\temporary folder" #
 Write-Host "`tDownloading the ns3.32-Ubuntu-20.04 from OneDrive`n" -ForegroundColor Yellow
 C:\temporary\wget.exe  -t 0 -O "C:\temporary\ns3-Ubuntu-20.04.7z" "https://ktokpa.sn.files.1drv.com/y4meXurSB3sCLcGjmCoWxCa2sQkgUtmQkuJ5iAbYwilWi_JCnSDMl3-0N73vDm9PoOgWu5JPzm7vAgBiUKobniW4eDKciHO9c7YMJb2dhPaVUS-h00k4xc5Cfe_8gYK20WvSwlIsofXUtbNdGBITTeNjD7H-8e47JkGlPg7mLIgxwcbw48z7yWZkIi2_KWmYhZ_sjHMAY9pb27nIZZ1deZHZA"
 
+}
+else {
+    # Execute this code if the file exists
+    Write-Host "File already exists."
+}
+
+$fileName = "TIMSCDR-Ubuntu-20.04.tar"
+$fileExists = Test-Path -Path (Join-Path $directoryPath $fileName)
+if (-not $fileExists) {
 # Extracting ns3-Ubuntu-20.04.7z to the C:\temporary #
 Write-Host "`tExtracting the download to C:\temporary`n" -ForegroundColor Yellow
 C:\temporary\7zr.exe x "C:\temporary\ns3-Ubuntu-20.04.7z" -o"C:\temporary" -y
+}else {
+    # Execute this code if the file exists
+    Write-Host "File already exists."
+}
 
+function Enable-WindowsOptionalFeature {
+    param (
+        [string]$featureName,
+        [string]$message
+    )
 
+    $feature = Get-WindowsOptionalFeature -Online | Where-Object { $_.FeatureName -eq $featureName }
+
+    if ($feature -and $feature.State -eq 'Enabled') {
+        Write-Host "`t$featureName is already enabled. Skipping.`n" -ForegroundColor Yellow
+    }
+    else {
+        Write-Host "`tEnabling $message`n" -ForegroundColor Yellow
+        dism.exe /online /enable-feature /featurename:$featureName /all /norestart
+    }
+}
 <# Checking and enabling pre-requisites (WSL and Virtual Machine Platform) #>
-Write-Host "`tEnabling Windows Subsystem Linux`n" -ForegroundColor Yellow
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-Write-Host "`tEnabling Virtual Machine Platform`n" -ForegroundColor Yellow
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-
-
-<# Setting up profile in windows Terminal #
-ignore these lines, its for testing purposes
-Write-Host "`tSetting up Ubuntu profile in Windows Terminal`n" -ForegroundColor Yellow
-# Path to the JSON file #
-$jsonFilePath = "C:\Users\$env:USERNAME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
-
-
-# Text to append
-$newProfile = @{
-    "name"              = "ns3-Ubuntu-20.04";
-    "colorScheme"       = "Ubuntu-ColorScheme";
-    "commandline"       = "wsl.exe -d ns3-Ubuntu-20.04";
-    "startingDirectory" = "~";
-    "icon"              = "ms-appx:///ProfileIcons/{9acb9455-ca41-5af7-950f-6bca1bc9722f}.png";
-}
-
-$colorScheme = @{
-    "background"          = "#300A24";
-    "black"               = "#171421";
-    "blue"                = "#0037DA";
-    "brightBlack"         = "#767676";
-    "brightBlue"          = "#08458F";
-    "brightCyan"          = "#2C9FB3";
-    "brightGreen"         = "#26A269";
-    "brightPurple"        = "#A347BA";
-    "brightRed"           = "#C01C28";
-    "brightWhite"         = "#F2F2F2";
-    "brightYellow"        = "#A2734C";
-    "cursorColor"         = "#FFFFFF";
-    "cyan"                = "#3A96DD";
-    "foreground"          = "#FFFFFF";
-    "green"               = "#26A269";
-    "name"                = "Ubuntu-ColorScheme";
-    "purple"              = "#881798";
-    "red"                 = "#C21A23";
-    "selectionBackground" = "#FFFFFF";
-    "white"               = "#CCCCCC";
-    "yellow"              = "#A2734C";
-}
-
-# Read the JSON file contents
-$jsonContent = Get-Content -Path $jsonFilePath | ConvertFrom-Json
-
-#appending color ubuntu scheme
-$jsonContent.schemes += $colorScheme
-
-#appending new ubuntu profile
-$jsonContent.profiles.list += $newProfile
-
-# Converting the updated JSON content back to a string
-$jsonString = $jsonContent | ConvertTo-Json -Depth 10
-
-# Writing the updated JSON string back to the file
-$jsonString | Out-File -FilePath $jsonFilePath -Encoding utf8 -Force
-
-<# 
-cp C:\Users\$env:USERNAME\Desktop\DELL-G3-wt-og.json C:\Users\$env:USERNAME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
-#>
+Enable-WindowsOptionalFeature -featureName "Microsoft-Windows-Subsystem-Linux" -message "Windows Subsystem for Linux"
+Enable-WindowsOptionalFeature -featureName "VirtualMachinePlatform" -message "Virtual Machine Platform"
 
 <# Installing WSL #>
 Write-Host "`tInstalling WSL`n" -ForegroundColor Yellow
-winget install --id 9P9TQF7MRM4R --accept-package-agreements
+$wingetCommand = Get-Command -Name 'winget' -ErrorAction SilentlyContinue
+
+if ($null -eq $wingetCommand) {
+    Write-Host "Windows Package Manager (winget) is not available. Please ensure it is installed and in your PATH."
+}
+else {
+    # Check if the package is already installed
+    $packageId = '9P9TQF7MRM4R'
+    $installedPackage = winget list --id $packageId | Select-String -Pattern $packageId
+
+    if ($installedPackage) {
+        Write-Host "Package with ID '$packageId' is already installed. Skipping installation."
+    }
+    else {
+        # Install the package
+        Write-Host "Installing the package..."
+        winget install --id $packageId --accept-package-agreements
+    }
+}
 
 <# Creating a scheduled task to continue the script #>
 # Get the current user's domain and username
@@ -165,7 +178,6 @@ if ($choice2 -eq 'y') {
 Restart-Computer
     }
 }
-
 else {
 
 Write-Host "Script Terminated" -ForegroundColor Red
